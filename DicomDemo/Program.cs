1df19b5b83a615f89625c3a67df0b5e016ebc435
subramanian <subramanian.enkay@gmail.com>
2024-07-21 08:56:52 +0530

    saving to an image

--- DicomDemo/Program.cs
+++ DicomDemo/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using FellowOakDicom;
+using FellowOakDicom.Imaging;
+using FellowOakDicom.Imaging.Render;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System.Drawing;
+
+var templatesFolder = @"C:\Users\subra\source\repos\DicomDotNet\DicomFiles\";
+
+
+using (var image = Aspose.Imaging.Image.Load(Path.Combine(templatesFolder, @"000003.dcm")))
+{
+    // Create an instance of JpegOptions
+    var exportOptions = new Aspose.Imaging.ImageOptions.JpegOptions();
+
+    // Save dicom to jpeg
+    image.Save(Path.Combine(templatesFolder, "output.jpeg"), exportOptions);
+
+    File.Delete(Path.Combine(templatesFolder, "output.jpeg"));
+}
+
+
+
+
