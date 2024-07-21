cea461bf241bb1fdb44f7505d5aff7efb59f949f
subramanian <subramanian.enkay@gmail.com>
2024-07-21 09:37:31 +0530

    modifying the patient name

--- DicomDemo/Program.cs
+++ DicomDemo/Program.cs
@@ -2,13 +2,15 @@
 
 using FellowOakDicom;
 using FellowOakDicom.Imaging;
+using FellowOakDicom.Imaging.Codec;
 using FellowOakDicom.Imaging.Render;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using System.Drawing;
 
 var templatesFolder = @"C:\Users\subra\source\repos\DicomDotNet\DicomFiles\";
 
-
+var x = Path.Combine(templatesFolder, @"output5.dcm");
+/*
 using (var image = Aspose.Imaging.Image.Load(Path.Combine(templatesFolder, @"000003.dcm")))
 {
     // Create an instance of JpegOptions
@@ -19,7 +21,21 @@ using (var image = Aspose.Imaging.Image.Load(Path.Combine(templatesFolder, @"000
 
     File.Delete(Path.Combine(templatesFolder, "output.jpeg"));
 }
+*/
 
 
 
 
+
+//var file = DicomFile.Open(x);             // Alt 1
+                                          
+
+var file = await DicomFile.OpenAsync(x);  // Alt 2
+
+var patientid = file.Dataset.GetString(DicomTag.PatientName);
+
+file.Dataset.AddOrUpdate(DicomTag.PatientName, "DOEJOHN");
+
+var g = file.Dataset.GetString(DicomTag.PatientName);
+file.Save(@"C:\Users\subra\source\repos\DicomDotNet\DicomFiles\output5.dcm");             // Alt 1
+await file.SaveAsync(@"output.dcm");
